29a102320b57f92584aaf4862b71979b1e104927
Muhammad Mizanur Rahman <55780952+mizan404@users.noreply.github.com>
2021-01-13 18:51:07 +0600

    13.01.2021_class2

--- Final Project File/hospital_v4/hospital/src/main/java/com/spring/maven/model/OnlineAppointment.java
+++ Final Project File/hospital_v4/hospital/src/main/java/com/spring/maven/model/OnlineAppointment.java
@@ -5,10 +5,53 @@
  */
 package com.spring.maven.model;
 
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  *
  * @author mohdm
  */
+@Entity
 public class OnlineAppointment {
-    
+
+    @Id
+    private int id;
+    private int patient_nid;
+    private String patient_problem;
+    private String AppointmentDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPatient_nid() {
+        return patient_nid;
+    }
+
+    public void setPatient_nid(int patient_nid) {
+        this.patient_nid = patient_nid;
+    }
+
+    public String getPatient_problem() {
+        return patient_problem;
+    }
+
+    public void setPatient_problem(String patient_problem) {
+        this.patient_problem = patient_problem;
+    }
+
+    public String getAppointmentDate() {
+        return AppointmentDate;
+    }
+
+    public void setAppointmentDate(String AppointmentDate) {
+        this.AppointmentDate = AppointmentDate;
+    }
+
 }
